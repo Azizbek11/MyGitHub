3e8100a9c6b20c5cef395988bc2e19c76f5676e6
Azizbek Ismoilov <azizismoilov495@gmail.com>
2021-04-14 17:43:11 +0500

    second comment

--- app/src/main/java/com/azizbek/mygithub/ui/popular/PopularRepoFragment.kt
+++ app/src/main/java/com/azizbek/mygithub/ui/popular/PopularRepoFragment.kt
@@ -1,10 +1,14 @@
 package com.azizbek.mygithub.ui.popular
 
+import android.app.Service
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +20,10 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.my_lodaer_item.*
 
 class PopularRepoFragment : Fragment() {
+
+    var connectivity : ConnectivityManager? = null
+    var info : NetworkInfo? = null
+    var isOnline=false
 
     private var avatarUrl: String?=null
 
@@ -29,19 +37,47 @@ class PopularRepoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val bundle= activity?.intent?.extras
-        if (bundle != null) {
-            avatarUrl = bundle.getString("photoUrl")
+     checkNet()
+
+    }
+
+    private fun checkNet() {
+
+        connectivity = context?.getSystemService(Service.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if ( connectivity != null) {
+            info = connectivity!!.activeNetworkInfo
+
+            if (info != null) {
+                if (info!!.state == NetworkInfo.State.CONNECTED) {
+                    isOnline = true
+                }
+            } else {
+                pushDialog()
+            }
+
+
+            val bundle = activity?.intent?.extras
+            if (bundle != null) {
+                avatarUrl = bundle.getString("photoUrl")
+            }
+
         }
+        if (isOnline){
+            isMain()
+        }
+    }
 
+    private fun isMain() {
 
+        dotsLoader.visibility = View.VISIBLE
         getData("kotlin")
 
         Glide.with(this)
-            .load(avatarUrl)
-            .placeholder(R.drawable.ic_baseline_account_circle_24)
-            .apply(RequestOptions().circleCrop())
-            .into(account_image)
+                .load(avatarUrl)
+                .placeholder(R.drawable.ic_baseline_account_circle_24)
+                .apply(RequestOptions().circleCrop())
+                .into(account_image)
 
         image_filter.setOnClickListener {
 
@@ -64,6 +100,23 @@ class PopularRepoFragment : Fragment() {
         }
     }
 
+    private fun pushDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(R.string.if_internet_net)
+        builder.setCancelable(false)
+        builder.setIcon(R.drawable.ic_baseline_network_check_24)
+        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+           checkNet()
+        }
+
+        builder.setNeutralButton(android.R.string.cancel) { dialog, which ->
+            activity?.finish()
+        }
+
+
+        builder.show()
+    }
+
     private fun getData(language:String){
         dotsLoader.visibility=View.VISIBLE
         home_recyclerView.visibility=View.GONE
@@ -80,4 +133,5 @@ class PopularRepoFragment : Fragment() {
         });
 
     }
+
 }
